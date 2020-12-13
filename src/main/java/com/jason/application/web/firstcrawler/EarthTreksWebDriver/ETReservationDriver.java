14dd5b44c3eb521f451ec636c8634f403c703723
verticalambition <jasonelish@gmail.com>
2020-12-12 17:28:57 -0700

    Put timers back in but shortened them. Also, modifying code to support ET new method of releasing all time slots at 11 a.m. 3 days before

--- src/main/java/com/jason/application/web/firstcrawler/EarthTreksWebDriver/ETReservationDriver.java
+++ src/main/java/com/jason/application/web/firstcrawler/EarthTreksWebDriver/ETReservationDriver.java
@@ -42,7 +42,7 @@ public class ETReservationDriver {
         //driver.get("file:///home/jason/IdeaProjects/applications/web/offlineHTML/app.rockgympro.com.html");
         //driver.get("file:///home/jason/IdeaProjects/applications/web/offlineHTML/app.rockgympro.commainwhenslotavail.html");
         driver.get("https://app.rockgympro.com/b/widget/?a=offering&offering_guid=0077362cf5a04cfc9150386cfc7e6c03&random=5fbae5621d2f8&iframeid=&mode=p");
-        //waitForNextAction(1000, 1500);
+        waitForNextAction(1000, 1500);
 
         //Select One Member
         driver.findElement(By.xpath("/html/body/div[1]/div/form/div[6]/div/fieldset/table/tbody/tr[1]/td[1]/a[2]")).click();
@@ -58,9 +58,10 @@ public class ETReservationDriver {
             //If we have the availability to still find something in this day
             System.out.println("This day has some openings");
             day.click();
-          //  waitForNextAction(500, 750);
+            waitForNextAction(500, 750);
             //Get the table that contains all the time slots
             WebElement timeSlotContainer = driver.findElement(By.xpath("/html/body/div[1]/div/form/fieldset/div[1]/table/tbody"));
+            //*[@id="offering-page-select-events-table"]/tbody
             //Get a List of individual time slot html tags
             List<WebElement> possibleTimeSlots = timeSlotContainer.findElements(By.tagName("tr"));
             //Look through slots to find time slot we want and then check if its bookable
@@ -68,7 +69,7 @@ public class ETReservationDriver {
                 if(slot.getText().contains(" " + myReservation.getTime() + " " + myReservation.getAmpm() + " to ") && slot.getText().contains("Select")) {
                     System.out.println(slot.getText());
                     slot.findElement(By.tagName("a")).click();
-            //        waitForNextAction(1200, 2300);
+                    waitForNextAction(1200, 2300);
                     System.out.println("Finished Processing Page One");
                     return processPageTwo();
                 }
@@ -82,44 +83,44 @@ public class ETReservationDriver {
 
         //First name
         driver.findElement(By.xpath("//*[@id=\"pfirstname-pindex-1-1\"]")).sendKeys(myReservation.getUserDetails().getFirstName());
-        //waitForNextAction(200, 300);
+        waitForNextAction(50, 100);
 
         //Last  name
         driver.findElement(By.xpath("//*[@id=\"plastname-pindex-1-1\"]")).sendKeys(myReservation.getUserDetails().getLastName());
-        //waitForNextAction(200, 300);
+        waitForNextAction(50, 100);
 
         //Middle Init
         driver.findElement(By.xpath("//*[@id=\"pmiddle-pindex-1-1\"]")).sendKeys(myReservation.getUserDetails().getMiddleName());
-        //waitForNextAction(200, 300);
+        waitForNextAction(50, 100);
 
         //Month Birth Day
         WebElement element = driver.findElement(By.xpath("//*[@id=\"participant-birth-pindex-1month\"]"));
         Select monthSelection = new Select(element);
         monthSelection.selectByValue(myReservation.getUserDetails().getBirthMonth());
-        //waitForNextAction(200, 300);
+        waitForNextAction(50, 100);
 
         //Day Birth Day
         element = driver.findElement(By.xpath("//*[@id=\"participant-birth-pindex-1day\"]"));
         Select daySelection = new Select(element);
         daySelection.selectByValue(myReservation.getUserDetails().getBirthDay());
-        //waitForNextAction(200, 300);
+        waitForNextAction(50, 100);
 
         //Year Birth Day
         element = driver.findElement(By.xpath("//*[@id=\"participant-birth-pindex-1year\"]"));
         Select yearSelection = new Select(element);
         yearSelection.selectByValue(myReservation.getUserDetails().getBirthYear());
-        //waitForNextAction(200, 300);
+        waitForNextAction(50, 100);
 
         //Check Intro to Roped Climbing Button
         driver.findElement(By.xpath("/html/body/div[1]/div/form/fieldset[2]/div[2]/span/input")).click();
-        //waitForNextAction(200, 300);
+        waitForNextAction(50, 100);
 
         //Check Cancellation Policy
         driver.findElement(By.xpath("/html/body/div[1]/div/form/fieldset[3]/div[2]/span/input")).click();
-        //waitForNextAction(200, 300);
+        waitForNextAction(50, 100);
         //Move on to next page
         driver.findElement(By.xpath("/html/body/div[1]/div/form/a[2]")).click();
-        //waitForNextAction(1000, 2000);
+        waitForNextAction(1000, 2000);
         System.out.println("Finished Processing Page Two");
         return processPageThree();
     }
@@ -128,19 +129,19 @@ public class ETReservationDriver {
         //driver.get("file:///home/jason/IdeaProjects/applications/web/offlineHTML/app.rockgympro.comconfirmation.html");
         //Enter Email address
         driver.findElement(By.xpath("//*[@id=\"customer-email\"]")).sendKeys(myReservation.getUserDetails().getEmail());
-        //waitForNextAction(200, 400);
+        waitForNextAction(50, 100);
 
         //Enter Phone Number
         driver.findElement(By.xpath("//*[@id=\"customer-phone\"]")).sendKeys(myReservation.getUserDetails().getPhoneNumber());
-        //waitForNextAction(200, 400);
+        waitForNextAction(50, 100);
 
         //Check box related to cancellation fee
         driver.findElement(By.xpath("//*[@id=\"theform\"]/fieldset[5]/div/strong[9]/span/input")).click();
-        //waitForNextAction(200, 400);
+        waitForNextAction(50, 100);
 
         //Complete the Booking
         driver.findElement(By.xpath("//*[@id=\"confirm_booking_button\"]")).click();
-        //waitForNextAction(3000, 5000);
+        waitForNextAction(3000, 5000);
         String pageSourceResponse = driver.getPageSource();
         driver.close();
         System.out.println("Finished Processing Page 3");
